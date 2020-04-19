7204edc22bbb9c1285816bc3156ed931e2023f2a
Renata Sarmet <renatassmendes@hotmail.com>
2020-04-19 15:21:22 -0300

    Fazendo tela de entrar e sair da fila

--- src/pages/ClienteFilasEstabelecimento.tsx
+++ src/pages/ClienteFilasEstabelecimento.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonPage, IonContent, IonButton, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonLabel, IonList, IonItem, IonNote, IonSearchbar, IonSegment, IonSegmentButton } from '@ionic/react'
+import { IonPage, IonContent, IonButton, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonLabel, IonList, IonItem, IonNote, IonSearchbar, IonSegment, IonSegmentButton, IonModal, IonIcon, IonInput } from '@ionic/react'
 import { withRouter, RouteComponentProps } from 'react-router';
 
 const style = { color: 'gray' }
@@ -17,7 +17,9 @@ class ClienteFilasEstabelecimento extends React.Component<CriarFilaInterface> {
         { 'id': '2', 'nome': 'Fila 2', 'distancia': '50min' }],
 
         'campo_busca': '',
-
+        'showModal': false,
+        'esta_na_fila': false,
+        'id_fila_escolhida': 1
     }
 
     filterList(filas: any[], busca: string) {
@@ -50,13 +52,53 @@ class ClienteFilasEstabelecimento extends React.Component<CriarFilaInterface> {
                             onIonChange={(e) => this.setState({ 'filas_filtradas': this.filterList(this.state.filas, e.detail.value!), 'campo_busca': e.detail.value! })}  >
                         </IonSearchbar>
 
-
                     </IonToolbar>
+
+
+                    <IonModal
+                        swipeToClose={true}
+                        isOpen={this.state.showModal}
+                        onDidDismiss={() => this.setState({ showModal: false })}>
+                        <IonHeader>
+                            <IonToolbar>
+                                <IonTitle style={style}>Estabelecimento {id}</IonTitle>
+
+                                <IonButton slot="end" onClick={() => this.setState({ showModal: false })}>
+                                    <IonIcon slot="icon-only" icon="close" />
+                                </IonButton>
+                            </IonToolbar>
+                        </IonHeader>
+                        <IonContent style={{ padding: 32, width: '100%', maxWidth: 768, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                            <br /><br />
+                            <IonTitle style={style}>Fila {this.state.id_fila_escolhida} </IonTitle>
+                            <div style={{ display: 'flex', flexDirection: 'column', margin: 40, justifyContent: 'center' }}>
+
+                                <div style={{ color: 'gray' }}>
+                                    <IonLabel>Pessoas na sua frente:</IonLabel>
+                                    <IonLabel>3</IonLabel>
+                                </div><br />
+                                <div style={{ color: 'gray', marginTop: 15 }}>
+                                    <IonLabel>Tempo estimado de espera: </IonLabel>
+                                    <IonLabel>45 min</IonLabel>
+                                </div><br />
+                                <div style={{ color: 'gray', marginTop: 15 }}>
+                                    <IonLabel>Descrição: </IonLabel>
+                                    <IonLabel>Aqui fica a descrição, explique sobre a fila aqui</IonLabel>
+                                </div> <br /><br /><br />
+                                {this.state.esta_na_fila ?
+                                    (<IonButton fill='solid' color='danger' onClick={() => this.setState({ esta_na_fila: false })}>Sair da Fila</IonButton>) :
+                                    (<IonButton fill='solid' color='secondary' onClick={() => this.setState({ esta_na_fila: true })}>Entrar na Fila</IonButton>)
+                                }
+                            </div>
+
+                        </IonContent>
+                    </IonModal>
+
 
                     <IonList>
                         {this.state.filas_filtradas.map(f => {
                             return (
-                                <IonItem button onClick={() => { }} color="">
+                                <IonItem button onClick={() => this.setState({ showModal: true, id_fila_escolhida: f.id })} color="">
                                     <IonLabel style={{ color: '#353839 ' }}>{f.nome}</IonLabel>
                                     <IonNote slot="end" style={{ fontSize: 13 }}>{f.distancia}</IonNote>
                                 </IonItem>
