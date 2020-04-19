d24415f98f0708fe23e20b83159d6cb1b8cb1f10
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-04-19 00:31:44 -0300

    Add route to login screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
+import Login from './pages/Login';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -28,7 +29,7 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path="/" component={Login} exact={true} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
